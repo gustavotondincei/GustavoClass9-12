4fa67a2c89357da93ef156897b29135e687be569
gustavotondincei <gus.tondin@alumnos.cei.es>
2026-02-24 11:57:28 +0100

    class 21

---
+++ Class 21/js/index.js
@@ -0,0 +1,8 @@
+console.log("Hola mundo")
+console.error("Esto es un error")
+console.info("Esto es un info")
+console.warn("Esto es un warn")
+console.clear()
+
+console.log("29+1")
+console.log(29+1)
