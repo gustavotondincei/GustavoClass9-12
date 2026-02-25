d2a50f3f02051a35f4740e86e5fc4fc989b6b5d5
gustavotondincei <gus.tondin@alumnos.cei.es>
2026-02-25 11:55:59 +0100

    Class 22

---
+++ Class 22/js/index.js
@@ -0,0 +1,41 @@
+// variables
+
+    // let
+    let nombreProfe = "Federico"
+    nombreProfe = "Pedro"
+
+    // const
+    const nombreAluno ="Gustavo"
+
+// console
+console.log(nombreProfe + " es el profe de " + nombreAluno)
+
+
+let nombre = "Pedro";
+    nombre = "Gustavo"; //(se puede cambiar el valor despues
+
+
+/*    
+    let suma = 2 + 2 // suma=4
+    let resta = 2 - 2 // resta=0
+    let multiplicación = 2 * 2 // multipl=4
+    let división = 5 / 2 // división=2.5
+    let módulo = 10 % 3 // módulo=1     (caben 3 numeros 3 dentro del 10 y sobra 1, que es el módulo)
+    let incremento = 0++ // incremento=1  (se puede utilizar para pasar imagens por ej., y hacer secciones mas interativas)
+*/
+
+
+let suma = 1 
+suma += 4 // suma=5
+
+let resta = 5 
+resta -= 2 // resta=3
+
+let multiplicación = 2
+multiplicación *= 4 // multiplicación=8
+
+let división = 6 
+división /= 2 // división=3
+
+let módulo = 10 
+módulo %= 2 // módulo=0
