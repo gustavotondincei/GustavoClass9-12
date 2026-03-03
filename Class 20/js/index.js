9f09e5993fbd824eb6c89898344854a7fbcbfb26
gustavotondincei <gus.tondin@alumnos.cei.es>
2026-03-03 11:55:41 +0100

    Class 25

---
+++ Class 20/js/index.js
@@ -0,0 +1,20 @@
+
+let popUp = document.getElementById('popup')
+let popMenu = document.getElementById('menu')
+
+
+function openPopup (){
+    popUp.classList.add('active')
+}
+
+function closePopup() {
+    popUp.classList.remove('active')
+}   
+
+function openMenu(){
+    popMenu.classList.add('active')
+}
+
+function closeMenu() {
+    popMenu.classList.remove('active')
+}  
